fa5c48481946b46431043282e86cde35376ae24f
XzJ <394386241@qq.com>
2019-05-26 16:36:28 +0800

    add 2.5 - represent pair a and b as is the product of 2**a * 3**b

--- exercise/2.1 introcution to data abstruction/2.5 what is meant by data.scm
+++ exercise/2.1 introcution to data abstruction/2.5 what is meant by data.scm
@@ -1,1 +1,24 @@
 #lang sicp
+;;根据构造函数cons的定义, 可以直接返回the product of 两个expt
+(define (cons a b)
+  (* (expt 2 a)
+     (expt 3 b)))
+
+
+(define (car x)
+  (if (= 0 (remainder x 2))
+      (+ 1 (car (/ x 2)))
+      0))
+
+
+
+(define (cdr x)
+  (if (= 0 (remainder x 3))
+      (+ 1 (car (/ x 3)))
+      0))
+
+;;test
+(define z (cons 2 3))
+(car z)
+(cdr z)
+         
