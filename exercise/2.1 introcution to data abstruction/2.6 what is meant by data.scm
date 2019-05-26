b1590a744e92e6cfcd920754829071d64df8a190
XzJ <394386241@qq.com>
2019-05-26 17:15:17 +0800

    church numerals, numeral can be represented by process

---
+++ exercise/2.1 introcution to data abstruction/2.6 what is meant by data.scm
@@ -0,0 +1,50 @@
+#lang sicp
+
+
+;;Church numerals
+(define zero
+  (lambda (f)
+    (lambda (x)
+      x)))
+
+(define (add-1 n)
+  (lambda (f)
+    (lambda (x)
+      (f ((n f) x)))))
+
+;;defination of ONE can get from (add-1 zero):
+(add-1 zero)
+
+(add-1 (lambda (f)
+         (lambda (x)
+           x)))
+
+((lambda (n)
+  (lambda (f)
+    (lambda (x)
+      (f ((n f) x)))))
+ (lambda (f)
+   (lambda (x)
+     x)))
+
+(define one
+  (lambda (f)
+    (lambda (x)
+      (f x))))
+
+(define two
+  (lambda (f)
+    (lambda (x)
+      (f (f x)))))
+
+one
+zero
+two
+
+;;define +
+(define +
+  (lambda (m)
+    (lambda (n)
+      (lambda (f)
+        (lambda (x)
+          (m f (n f x)))))))
