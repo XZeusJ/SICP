91ff6fba98f2312f72258c5de6e3ade48352412c
XzJ <394386241@qq.com>
2019-05-30 23:25:30 +0800

    new files update

--- exercise/1.2 procedures and processes they generate/1.10 linear recursion and iteration.scm
+++ exercise/1.2 procedures and processes they generate/1.10 linear recursion and iteration.scm
@@ -29,11 +29,12 @@
 2**10
 |#
 
-(define (f n) (A 0 n))
+
 
 
 ;; a version about display a sequence
 ;; we can use this display process to see what (f n) would print out
+#|
 (define (show n)
   (define (iter n counter)
     (if (> counter n) 'liftoff
@@ -44,15 +45,27 @@
                 (+ counter 1)))))
   (iter 10 0))
 (show 10)
-
-;; another version about display a sequence
-#|
-(let countdown ((i 10))
- (if (= i 0)
-     'liftoff
-      (begin
-        (display i)
-        (newline)
-        (countdown (- i 1)))))
 |#
 
+(define (show-function f)
+  (define (iter f n counter)
+    (if (> counter n) 'liftoff
+        (begin
+          (display counter)
+          (display " ")
+          (display (f counter))
+          (display "\n")
+          (iter f n
+                (+ counter 1)))))
+  (iter f 10 0))
+
+; TEST
+(define (f n) (A 0 n))
+(define (g n) (A 1 n))
+(define (h n) (A 2 n))
+(define (k n) (* 5 n n))
+
+(show-function f) ;; (f n) computes 2*n
+(show-function g) ;; (g n) computes 2**n
+;(show-function h) ;; (h n) computes 2**(2**n)
+(show-function k) ;; (k n) computes 5*n**2
